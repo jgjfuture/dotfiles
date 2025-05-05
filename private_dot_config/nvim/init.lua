18b1e4e374595d698ad73759f0283f9f469efb00
jgjfuture <jgjfuture@outlook.jp>
2025-05-05 22:46:30 +0900

    VSCode NeoVim用の設定を追加

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,5 +1,4 @@
 -- Buffer
-vim.opt.relativenumber = true
 vim.opt.hlsearch = true
 
 -- Command mode
@@ -21,5 +20,21 @@ vim.keymap.set('v', '<C-k>', '5k', { noremap = true })
 
 -- Plugin
 -- Enable matchit plugin
-vim.cmd('packadd! matchit')
+-- vim.cmd('packadd! matchit')
 
+-- Environment Specific Configs
+if vim.g.vscode then
+  local vscode = require('vscode')
+  vim.keymap.set('n', '==', function() vscode.action("editor.action.formatDocument") end)
+  vim.keymap.set('n', '[c', function() vscode.action("workbench.action.editor.nextChange") end)
+  vim.keymap.set('n', ']c', function() vscode.action("workbench.action.editor.previousChange") end)
+  vim.keymap.set('n', '[e', function() vscode.action("workbench.action.marker.nextInFiles") end)
+  vim.keymap.set('n', ']e', function() vscode.action("workbench.action.marker.prevInFiles") end)
+  vim.keymap.set('n', '<Space>', function() vscode.action("workbench.action.showCommands") end)
+  vim.keymap.set('n', '<C-I>', function() vscode.action("editor.action.goToImplementation") end)
+
+  vim.opt.clipboard:append("unnamedplus")
+else
+  -- only for ordinal neovim
+  vim.opt.relativenumber = true
+end
