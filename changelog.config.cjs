92351d192705cdba2a97be3d12897452f91c2da1
jgjfuture <jgjfuture@outlook.jp>
2025-05-28 14:39:56 +0900

    chore(general): リポジトリにgit-czの設定を追加

---
+++ changelog.config.cjs
@@ -0,0 +1,52 @@
+module.exports = {
+    disableEmoji: true,
+    format: '{type}{scope}: {emoji}{subject}',
+    list: ['feat', 'fix', 'chore', 'docs', 'refactor', 'style', 'ci', 'perf'],
+    maxMessageLength: 64,
+    minMessageLength: 3,
+    questions: ['type', 'scope', 'subject', 'body'],
+    scopes: ['editor', 'shell', 'apps', 'chezmoi', 'general'],
+    types: {
+        chore: {
+            description: 'Build process or auxiliary tool changes',
+            value: 'chore'
+        },
+        ci: {
+            description: 'CI related changes',
+            value: 'ci'
+        },
+        docs: {
+            description: 'Documentation only changes',
+            value: 'docs'
+        },
+        feat: {
+            description: 'A new feature',
+            value: 'feat'
+        },
+        fix: {
+            description: 'A bug fix',
+            value: 'fix'
+        },
+        perf: {
+            description: 'A code change that improves performance',
+            value: 'perf'
+        },
+        refactor: {
+            description: 'A code change that neither fixes a bug or adds a feature',
+            value: 'refactor'
+        },
+        style: {
+            description: 'Markup, white-space, formatting, missing semi-colons...',
+            value: 'style'
+        },
+        messages: {
+            type: 'Select the type of change that you\'re committing:',
+            customScope: 'Select the scope this component affects:',
+            subject: 'Write a short, imperative mood description of the change:\n',
+            body: 'Provide a longer description of the change:\n ',
+            breaking: 'List any breaking changes:\n',
+            footer: 'Issues this commit closes, e.g #123:',
+            confirmCommit: 'The packages that this commit has affected\n',
+        },
+    }
+};
