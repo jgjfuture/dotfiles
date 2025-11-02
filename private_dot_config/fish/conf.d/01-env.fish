095df410b8df0598ea2b133b2739de10568af801
jgjfuture <jgjfuture@outlook.jp>
2025-11-03 01:33:40 +0900

    chore(shell): npmbinの設定はchezmoi管理外にする

--- private_dot_config/fish/conf.d/01-env.fish
+++ private_dot_config/fish/conf.d/01-env.fish
@@ -1,9 +1,6 @@
 if status --is-login
   eval "$(/opt/homebrew/bin/brew shellenv)"
   
-  # to avoid global npm package installation, make npmbin on home directory
-  fish_add_path $HOME/npmbin/node_modules/.bin:$PATH
-  
   # 1Password SSH Setting
   set -x SSH_AUTH_SOCK ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
   
