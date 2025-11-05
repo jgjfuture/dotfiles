d067d9551c88d5cc8977f79a421ace41912973f9
jgjfuture <jgjfuture@outlook.jp>
2025-11-05 23:09:30 +0900

    style(editor): VSCode Neovimで背景色がつかないようにテーマの読み込みを分岐する

--- private_dot_config/nvim/lua/plugins/cobalt2.lua
+++ private_dot_config/nvim/lua/plugins/cobalt2.lua
@@ -3,7 +3,9 @@ return {
     event = { "ColorSchemePre" }, -- if you want to lazy load
     dependencies = { "tjdevries/colorbuddy.nvim", tag = "v1.0.0" },
     init = function()
-        require("colorbuddy").colorscheme("cobalt2")
+	if not vim.g.vscode then
+	  require("colorbuddy").colorscheme("cobalt2")
+        end
     end,
 }
 
