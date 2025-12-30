c7072e5732d328991b830adb8e1da4d7df96cfcd
jgjfuture <jgjfuture@outlook.jp>
2025-12-30 18:24:50 +0900

    chore(editor): nvim autosave on focus lost

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -66,5 +66,11 @@ else
   vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
   vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
   vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+  -- autosave settings
+  vim.api.nvim_create_autocmd({ "FocusLost", "BufLeave" }, {
+    pattern = "*",
+    command = "silent! wall"
+  })
 end
 
